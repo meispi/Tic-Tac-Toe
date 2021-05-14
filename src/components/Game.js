5f2770f330e8dcf17344ba7302967d3c98d6b6f3
Saurabh Tiwari <tsaurabh2001@gmail.com>
2021-05-14 22:42:17 +0530

    slight changes in game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -12,23 +12,28 @@ export default function Game(props) {
   ]);
 
   function checkWinner() {
+    console.log(matrix)
     for (let i = 0; i < 3; i++) {
       if (matrix[i][0] === matrix[i][1] && matrix[i][1] === matrix[i][2]) {
-        if (matrix[i][2] === 1) return 1;
-        else if (matrix[i][2] === 0) return 0;
+        return matrix[i][2];
+        // if (matrix[i][2] === 1) return 1;
+        // else if (matrix[i][2] === 0) return 0;
       }
       if (matrix[0][i] === matrix[1][i] && matrix[1][i] === matrix[2][i]) {
-        if (matrix[2][i] === 1) return 1;
-        else if (matrix[2][i] === 0) return 0;
+        return matrix[2][i];
+        // if (matrix[2][i] === 1) return 1;
+        // else if (matrix[2][i] === 0) return 0;
       }
     }
     if (matrix[0][0] === matrix[1][1] && matrix[1][1] === matrix[2][2]) {
-      if (matrix[2][2] === 1) return 1;
-      else if (matrix[2][2] === 0) return 0;
+      return matrix[2][2];
+      // if (matrix[2][2] === 1) return 1;
+      // else if (matrix[2][2] === 0) return 0;
     }
     if (matrix[0][2] === matrix[1][1] && matrix[1][1] === matrix[2][0]) {
-      if (matrix[2][0] === 1) return 1;
-      else if (matrix[2][0] === 0) return 0;
+      return matrix[2][0];
+      // if (matrix[2][0] === 1) return 1;
+      // else if (matrix[2][0] === 0) return 0;
     }
     return -1;
   }
